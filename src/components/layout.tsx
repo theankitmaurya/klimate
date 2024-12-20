a825402c3cc450f0656c564ff76e2f8316f3772c
Ankit <ankitmaurya20223@gmail.com>
2024-12-20 11:52:53 +0530

    Added social media links

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,8 +9,21 @@ export function Layout({ children }: PropsWithChildren) {
         {children}
       </main>
       <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
-        <div className="container mx-auto px-4 text-center text-gray-200">
-          <p>Made by Ankit</p>
+        <div className="container mx-auto px-4 text-center mt-2">
+          <p>Klimate | Coded by Ankit</p>
+          <p>Copyright © All Rights Reserved 2024-2025</p>
+          <div className="flex items-center justify-center gap-2">
+            <p>Follow Me At - </p>
+            <a href="https://www.instagram.com/theankitmaurya_">
+              <img src="/instagram.png" alt="" height={23} width={23} />
+            </a>
+            <a href="https://github.com/theankitmaurya">
+              <img src="/github.png" alt="" height={23} width={23} />
+            </a>
+            <a href="https://www.linkedin.com/in/theankitmauryaa/">
+              <img src="/linkedin.png" alt="" height={23} width={23} />
+            </a>
+          </div>
         </div>
       </footer>
     </div>
