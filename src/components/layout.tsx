26b7a457d059554bd42a740d48b2a33021c30f0a
Ankit Maurya <ankitmaurya20223@gmail.com>
2025-01-10 15:49:15 +0530

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,20 +10,8 @@ export function Layout({ children }: PropsWithChildren) {
       </main>
       <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
         <div className="container mx-auto px-4 text-center mt-2">
-          <p>Klimate | Coded by Ankit</p>
+          <p>Klimate</p>
           <p>Copyright © All Rights Reserved 2024-2025</p>
-          <div className="flex items-center justify-center gap-2">
-            <p>Follow Me At - </p>
-            <a href="https://www.instagram.com/theankitmaurya_">
-              <img src="/instagram.png" alt="" height={23} width={23} />
-            </a>
-            <a href="https://github.com/theankitmaurya">
-              <img src="/github.png" alt="" height={23} width={23} />
-            </a>
-            <a href="https://www.linkedin.com/in/theankitmauryaa/">
-              <img src="/linkedin.png" alt="" height={23} width={23} />
-            </a>
-          </div>
         </div>
       </footer>
     </div>
